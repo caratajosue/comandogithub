6274b21c5372d26f1c17e1af357761057f13d8fc
caratajosue <caratajosue@yahoo.com>
2020-08-24 19:21:31 -0300

    Primer

---
+++ suma/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero1;
+    int numero2;
+    int suma;
+    printf("Ingrse numero uno:\n");
+    scanf("%d",&numero1);
+    printf("Ingrse numero dos:\n");
+    scanf("%d",&numero2);
+    suma = numero1 + numero2;
+    printf("suma:%d\n",suma);
+    return 0;
+}
